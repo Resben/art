96f2515a908ef6a92fa596282dedee16d0b280cd
Resben <bendavidhawkins@gmail.com>
2024-05-05 20:42:11 +0800

    [Feature] Basic design layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,25 @@
 import React from "react";
 import "../css/General.css";
+import pic1 from "../assets/resben.png"
+import pic2 from "../assets/dev.png"
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 function Header() {
   return (
 
     <Container className="header-container">
     <Grid container className="header-grid" spacing={2}>
-        <Grid item xs={4}>
-          <h5 className="header-title">Resben Art</h5>
-        </Grid>
-        <Grid item xs={4}>
-          <a className="link" href="https://www.instagram.com/resbenart/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-          <a className="link" href="https://twitter.com/ResbenArt" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
-          <a className="link" href="https://www.tiktok.com/@resbenart" target="_blank"><i class="fa-brands fa-tiktok"></i></a>
-          <a className="link" href="https://resben.itch.io/" target="_blank"><i class="fa-brands fa-itch-io"></i></a>
-        </Grid>
-        <Grid item xs={4}>
-          <h1 className="header-title"></h1>
-        </Grid>
+      <Grid item xs={4}>
+        <img className="image" src={pic1}></img>
       </Grid>
+      <Grid item xs={4}>
+      </Grid>
+      <Grid item xs={4}>
+        <img className="image" src={pic2}></img>
+      </Grid>
+    </Grid>
     </Container>
   );
 }
--- src/control/App.js
+++ src/control/App.js
@@ -2,13 +2,16 @@ import '../css/App.css';
 import Header from '../components/Header';
 import Page from '../components/Page';
 import Footer from '../components/Footer';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className="background">
+      <Container className="main-container">
         <Header />
         <Page />
         <Footer />
+      </Container>
     </div>
   );
 }
